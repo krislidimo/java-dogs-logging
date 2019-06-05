e5432c076aa2c51f85be49fd3513e557a693bfe5
Krisli Dimo <krislidimo@gmail.com>
2019-06-05 17:17:54 -0500

    added custom logiing when api calls are made

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogController.java
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogController.java
@@ -1,5 +1,7 @@
 package com.lambdaschool.dogsinitial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,27 +13,28 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/dogs")
-public class DogController
-{
+public class DogController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DogController.class);
     // localhost:8080/dogs/dogs
     @GetMapping(value = "/dogs")
-    public ResponseEntity<?> getAllDogs()
-    {
+    public ResponseEntity<?> getAllDogs() {
+        logger.info("/dogs/dogs accessed");
         return new ResponseEntity<>(DogsinitialApplication.ourDogList.dogList, HttpStatus.OK);
     }
 
     // localhost:8080/dogs/{id}
     @GetMapping(value = "/{id}")
-    public ResponseEntity<?> getDogDetail(@PathVariable long id)
-    {
+    public ResponseEntity<?> getDogDetail(@PathVariable long id) {
+        logger.info("/dogs/" + id + " accessed");
         Dog rtnDog = DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id));
         return new ResponseEntity<>(rtnDog, HttpStatus.OK);
     }
 
     // localhost:8080/dogs/breeds/{breed}
     @GetMapping(value = "/breeds/{breed}")
-    public ResponseEntity<?> getDogBreeds (@PathVariable String breed)
-    {
+    public ResponseEntity<?> getDogBreeds (@PathVariable String breed) {
+        logger.info("/breeds/" + breed + " accessed");
         ArrayList<Dog> rtnDogs = DogsinitialApplication.ourDogList.
                 findDogs(d -> d.getBreed().toUpperCase().equals(breed.toUpperCase()));
         return new ResponseEntity<>(rtnDogs, HttpStatus.OK);
---
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/MessageDetail.java
@@ -0,0 +1,4 @@
+package com.lambdaschool.dogsinitial;
+
+public class MessageDetail {
+}
